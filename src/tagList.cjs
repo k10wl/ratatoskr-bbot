578ddc3a59ae83ea847f8e039498a0bdaf430d0e
k10wl <72667957+k10wl@users.noreply.github.com>
2021-06-15 13:36:59 +0300

    telegraf version

---
+++ src/tagList.cjs
@@ -0,0 +1,135 @@
+exports.tagList = [
+  {
+    name: "Жидкости, следы, фетиши",
+    groupTags: [
+      "#blood",
+      "#saliva",
+      "#bruises",
+      "#marks",
+      "#fetish",
+      "#costume",
+      "#stockings",
+      "#fishnet",
+      "#gloves",
+      "#lingerie",
+      "#latex",
+      "#fuck_u_leatherman",
+    ]
+  },
+  {
+    name: "Практики",
+    groupTags: [
+      "#bondage",
+      "#petplay",
+      "#piercingplay",
+      "#niddleplay",
+      "#waxplay",
+      "#waterplay",
+      "#breathplay",
+      "#forsedorgasm",
+      "#orgasmcontrol",
+      "#handjob",
+      "#footjob",
+      "#pegging",
+      "#suck",
+      "#spanking",
+      "#oral",
+      "#crossdress ",
+      "#trampling",
+      "#deprivation",
+      "#primal",
+      "#knifeplay",
+      "#fingering",
+      "#rubbing",
+      "#bite"
+    ]
+  },
+  {
+    name: "Девайсы, предметы",
+    groupTags: [
+      "#rope",
+      "#clothespins",
+      "#cuffs",
+      "#handcuffs",
+      "#legcuffs",
+      "#handbags",
+      "#collar",
+      "#gag",
+      "#muzzle",
+      "#strut",
+      "#buttplug",
+      "#furniture",
+      "#harness ",
+      "#fork ",
+      "#chastitybelt ",
+      "#bamboo",
+      "#ridingcrop"
+    ]
+  },
+  {
+    name: "Процесс",
+    groupTags: [
+      "#aftercare",
+      "#dominance",
+      "#submission"
+    ]
+  },
+  {
+    name: "Дополнительно",
+    groupTags: [
+      "#atmospheric",
+      "#tenderness",
+      "#cute",
+      "#art",
+      "#arms",
+      "#butt",
+      "#legs",
+      "#plants",
+      "#toys"
+    ]
+  },
+  {
+    name: "Участники",
+    groupTags: [
+      "#female",
+      "#male",
+      "#couple",
+      "#camdamage",
+      "#cuttlefish",
+      "#vexashley",
+      "#fourchambers",
+      "#kaijafaerie",
+      "#only_lovers_left_alive",
+      "#mcconaughey",
+      "#tilllindemann",
+      "#bradpitt",
+      "#marilynmanson",
+      "#davidtennant",
+      "#madsmikkelsen",
+      "#robertpattinson",
+      "#damianodavid",
+      "#heisenberg",
+      "#RE8"
+    ]
+  },
+  {
+    name: "Кинклист",
+    groupTags: [
+      "#kinklist"
+    ]
+  },
+  {
+    name: "Фотокниги",
+    groupTags: [
+      "#photobook",
+      "#polaroid"
+    ]
+  },
+  {
+    name: "Мемесы",
+    groupTags: [
+      "#memes",
+      "#длясправочек"
+    ]
+  },
+];
