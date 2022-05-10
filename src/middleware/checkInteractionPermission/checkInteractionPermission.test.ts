7c72a6e874e4f6f3db5ef3d7c3947d800058e12f
k10wl <y.kunytskyy@gmail.com>
2022-05-11 02:34:11 +0300

    refactored interaction permission middleware to work with mongo

--- src/middleware/checkInteractionPermission/checkInteractionPermission.test.ts
+++ src/middleware/checkInteractionPermission/checkInteractionPermission.test.ts
@@ -2,47 +2,48 @@ import { Context } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
 
 import { BOT_MESSAGES } from "@src/constants";
+import * as services from "@src/services";
 
-import {
-  checkInteractionPermission,
-  HAS_INTERACTION_PERMISSION,
-} from "./index";
+import { checkInteractionPermission } from "./index";
+
+jest.mock("@src/services");
+const mockedService = services as jest.Mocked<typeof services>;
 
 const mockTgReply = jest.fn();
-const mockTgContext = { reply: mockTgReply } as unknown as Context<Update>;
 const mockTgNext = jest.fn();
 
-describe("senderCanInteract", () => {
-  const middlewareFn = checkInteractionPermission();
+const mockTgContext = { reply: mockTgReply } as unknown as Context<Update>;
 
+describe("senderCanInteract", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
-  test("should only call 'reply' function with CANT_INTERACT_MESSAGE when 'ctx.from' is not defined", () => {
-    void middlewareFn(mockTgContext, mockTgNext);
+  test("should reply with rejection message and stop interaction when user is not found.", async () => {
+    mockedService.findOneRegisteredUserById = jest.fn().mockResolvedValue(null);
+
+    const testedFn = checkInteractionPermission();
+    await testedFn(mockTgContext, mockTgNext);
+
     expect(mockTgNext).not.toBeCalled();
     expect(mockTgReply).toBeCalledWith(BOT_MESSAGES.CANT_INTERACT_MESSAGE);
   });
 
-  test("should only call 'reply' function with CANT_INTERACT_MESSAGE when 'ctx.from' is unacceptable", () => {
-    void middlewareFn(
-      { ...mockTgContext, from: { id: 0 } } as Context<Update>,
-      mockTgNext
-    );
-    expect(mockTgNext).not.toBeCalled();
-    expect(mockTgReply).toBeCalledWith(BOT_MESSAGES.CANT_INTERACT_MESSAGE);
-  });
+  test("should proceed interaction without replying when user with given ID is found.", async () => {
+    const TEST_ID = 1234567890;
 
-  test("should only call 'next' function when user is acceptable", () => {
-    void middlewareFn(
-      {
-        ...mockTgContext,
-        from: { id: HAS_INTERACTION_PERMISSION[0] },
-      } as Context<Update>,
+    mockedService.findOneRegisteredUserById = jest
+      .fn()
+      .mockResolvedValue({ _id: "val", telegraf_user_id: TEST_ID });
+
+    const testedFn = checkInteractionPermission();
+    await testedFn(
+      { ...mockTgContext, from: { id: TEST_ID } } as Context<Update>,
       mockTgNext
     );
+
+    expect(mockedService.findOneRegisteredUserById).toBeCalledWith(TEST_ID);
     expect(mockTgNext).toBeCalled();
-    expect(mockTgReply).not.toBeCalled();
+    expect(mockTgReply).not.toBeCalledWith(BOT_MESSAGES.CANT_INTERACT_MESSAGE);
   });
 });
--- src/middleware/checkInteractionPermission/index.ts
+++ src/middleware/checkInteractionPermission/index.ts
@@ -2,13 +2,13 @@ import { Context, MiddlewareFn } from "telegraf";
 import { Update } from "telegraf/typings/core/types/typegram";
 
 import { BOT_MESSAGES } from "@src/constants";
-
-// TODO: create service that will get all known users array from DB.
-export const HAS_INTERACTION_PERMISSION: number[] = [510632907, 594124328];
+import { findOneRegisteredUserById } from "@src/services";
 
 export function checkInteractionPermission(): MiddlewareFn<Context<Update>> {
-  return (ctx, next) => {
-    if (ctx.from?.id && HAS_INTERACTION_PERMISSION.includes(ctx.from.id)) {
+  return async (ctx, next) => {
+    const canInteract = await findOneRegisteredUserById(ctx.from?.id);
+
+    if (canInteract) {
       return next();
     }
 
